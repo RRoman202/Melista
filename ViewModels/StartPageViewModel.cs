1409434eb6f678ed34af3b6e4ce23102cf6bbcc5
RRoman202 <romanrrr2005rrr@gmail.com>
2023-03-01 16:28:39 +0500

    commit3

--- ViewModels/StartPageViewModel.cs
+++ ViewModels/StartPageViewModel.cs
@@ -2,6 +2,11 @@
 {
     public class StartPageViewModel : BindableBase
     {
-
+        private readonly PageService _pageService;
+        public StartPageViewModel(PageService pageService)
+        {
+            
+            _pageService = pageService;
+        }
     }
 }
