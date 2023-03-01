bb2c5b42b7a20095ad2ed9bf83c9fe33fed58cf9
RRoman202 <romanrrr2005rrr@gmail.com>
2023-03-01 08:38:22 +0500

    1

---
+++ Services/PageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Melista.Services
+{
+    public class PageService
+    {
+        public event Action<Page>? onPageChanged;
+        public void ChangePage(Page page) => onPageChanged?.Invoke(page);
+    }
+}
