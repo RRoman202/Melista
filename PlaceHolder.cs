576baf4169fa79e9ac5dfc226653bed8831e9697
RRoman202 <romanrrr2005rrr@gmail.com>
2023-03-03 09:18:20 +0500

    placeholder

---
+++ PlaceHolder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melista
+{
+    public class PlaceHolderTextBox : TextBox
+    {
+        bool isPlaceHolder = true;
+        string _placeHolderText;
+        public string PlaceHolderText
+        {
+            get { return _placeHolderText; }
+            set
+            {
+                _placeHolderText = value;
+                setPlaceholder();
+            }
+        }
+
+        public new string Text
+        {
+            get => isPlaceHolder ? string.Empty : base.Text;
+            set => base.Text = value;
+        }
+
+        //when the control loses focus, the placeholder is shown
+        private void setPlaceholder()
+        {
+            if (string.IsNullOrEmpty(base.Text))
+            {
+                base.Text = PlaceHolderText;
+                isPlaceHolder = true;
+            }
+        }
+
+        //when the control is focused, the placeholder is removed
+        private void removePlaceHolder()
+        {
+
+            if (isPlaceHolder)
+            {
+                base.Text = "";
+
+                isPlaceHolder = false;
+            }
+        }
+        public PlaceHolderTextBox()
+        {
+            GotFocus += removePlaceHolder;
+            LostFocus += setPlaceholder;
+        }
+
+        private void setPlaceholder(object sender, EventArgs e)
+        {
+            setPlaceholder();
+        }
+
+        private void removePlaceHolder(object sender, EventArgs e)
+        {
+            removePlaceHolder();
+        }
+    }
+}
