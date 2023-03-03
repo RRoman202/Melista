94a08b6cefe88a67dbb7dacb545db65604831d99
Anoxx <petrovalexei2004103@mail.ru>
2023-03-03 11:05:00 +0500

    Добавил только открытие видео пока

--- ViewModels/StartPageViewModel.cs
+++ ViewModels/StartPageViewModel.cs
@@ -1,12 +1,30 @@
-﻿namespace Melista.ViewModels
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Melista.ViewModels
 {
     public class StartPageViewModel : BindableBase
     {
         private readonly PageService _pageService;
         public StartPageViewModel(PageService pageService)
         {
-            
             _pageService = pageService;
         }
+        public AsyncCommand LoadNewFileCommand => new(async () =>
+        {
+            await Task.Run(async () =>
+            {
+                OpenFileDialog OpenFile = new OpenFileDialog();
+                OpenFile.Filter = "Файлы mp3; mp4|*.mp3;*.mp4";
+                if (OpenFile.ShowDialog() == true)
+                {
+                    Process.Start(new ProcessStartInfo() { FileName = OpenFile.FileName, UseShellExecute = true });
+                }
+            });
+        }, bool () => true);
+        
+       
     }
 }
