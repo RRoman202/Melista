cd470b09a51ec8e929ce3a07e9bc9931c6c71df5
Diesel <92308937+XBOCT-XACHY@users.noreply.github.com>
2023-03-13 15:22:41 +0500

    bild1

---
+++ Global.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melista
+{
+    public static class Global
+    {
+        public static string CurrentMedia { get; set; }
+    }
+}
