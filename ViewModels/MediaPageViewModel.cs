79ee30bef5f3f1ac3c6cb2731b471208a15b2f25
Diesel <92308937+XBOCT-XACHY@users.noreply.github.com>
2023-03-20 16:04:45 +0500

    navigation timer added

--- ViewModels/MediaPageViewModel.cs
+++ ViewModels/MediaPageViewModel.cs
@@ -2,17 +2,26 @@
 using Melista.Models;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Threading;
+using System.Timers;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace Melista.ViewModels
 {
     public class MediaPageViewModel : BindableBase
     {
         private readonly PageService _pageService;
-
+        DispatcherTimer timer;
         public MediaPageViewModel(PageService pageService)
         {
+            timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) }; // 1 секунда
+            timer.Tick += Timer_Tick;
+
             _pageService = pageService;
+            InterfaceVisible = Visibility.Hidden;
             Player = new MediaElement()
             {
                 LoadedBehavior = MediaState.Manual,
@@ -25,10 +34,12 @@ namespace Melista.ViewModels
             }
         }
         public MediaElement Player { get; set; }
+        public Visibility InterfaceVisible { get; set; }
         public DelegateCommand Back => new(() =>
         {
             _pageService.ChangePage(new StartPageView());
         });
+
         public DelegateCommand PlayVideoCommand => new(() =>
         {
             string path = GetPathFromLink(Global.CurrentMedia.Path);
@@ -55,5 +66,29 @@ namespace Melista.ViewModels
                 return null;
             }
         }
+
+        public DelegateCommand NavigateCommand => new(() => InterfaceisVisible());
+
+        int NavigateTimer = 0;
+        public void InterfaceisVisible()
+        {
+            InterfaceVisible = Visibility.Visible;
+            NavigateTimer = 2;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            if (NavigateTimer == 0)
+            {
+                InterfaceVisible = Visibility.Hidden;
+            }
+
+            else
+            {
+                NavigateTimer--;
+            }
+        }
+
     }
 }
