8d7818a87bed068748b6c6bc4c6cad055ceaedb4
Almaz <murka.almazik@yandex.ru>
2023-04-24 14:11:15 +0500

    Settings + Speed video

--- ViewModels/MusicPageViewModel.cs
+++ ViewModels/MusicPageViewModel.cs
@@ -25,7 +25,8 @@ namespace Melista.ViewModels
 
             _windowService = windowService;
             _pageService = pageService;
-           
+            SettingsVisibility = Visibility.Hidden;
+
 
         }
         public string GetPathFromLink(string linkPathName)
@@ -56,6 +57,21 @@ namespace Melista.ViewModels
             });
             _pageService.ChangePage(new StartPageView());
         });
+
+        public Visibility SettingsVisibility { get; set; }
+        public DelegateCommand NavigateCommand2 => new(() => SettingsIsVisibility());
+
+        public void SettingsIsVisibility()
+        {
+            if(SettingsVisibility == Visibility.Hidden)
+            {
+                SettingsVisibility = Visibility.Visible;
+            }
+            else
+            {
+                SettingsVisibility = Visibility.Hidden;
+            }
+        }
 
     }
 }
